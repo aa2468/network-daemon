1d0864c2a63554095af7fcfef5dc2c19d1787c48
Amm752 <amm752@nau.edu>
2015-04-26 20:00:28 -0700

    added the printf

--- client.c
+++ client.c
@@ -1,4 +1,4 @@
-/* 
+/*
  work done by:
  Luis Valenzuela
  Abdulaziz Alhawas
@@ -16,27 +16,27 @@ int main(int argc, char** argv) {
     char input[100];
     int client_socket;
     struct sockaddr_in client_address;
-    
+
     //create unammed socket and name it
     client_socket = socket(AF_INET, SOCK_STREAM, 0);
     // create addr struct
     client_address.sin_family = AF_INET;
     client_address.sin_addr.s_addr = inet_addr(SERVER_ADDR);
     client_address.sin_port = htons(PORT);
-    
+
     // connect to server
     if (connect(client_socket, (struct sockaddr*) &client_address,sizeof(client_address)) == -1) {
         perror("error connecting to server\n");
         exit(EXIT_FAILURE);
     }
-    
+
     while (1) {
         printf("input : ");
-        
+
         // get the string
         fgets(input, sizeof(input), stdin);
         int i =0;
-        
+
         while (*(input + i)) {
             //request from server
             write(client_socket, input, sizeof(char));
@@ -45,14 +45,16 @@ int main(int argc, char** argv) {
                 close(client_socket);
                 printf("Done \n");
                 exit(EXIT_SUCCESS);
-                
+
             }
             read(client_socket, input , sizeof(char));
+            printf( "%c\n", output );
             i++;
-            
+
         }
-        
-        
+        else
+            break;
+
     }
-    
+    exit(EXIT_SUCCESS);
 }
